e9ef32175608300def87c022e31fa5116925f67e
s18629 <57294801+s18629@users.noreply.github.com>
2021-07-03 13:34:07 +0200

    Initial commit

---
+++ src/main/java/com/example/jazS_18629nbp/JazS18629NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazS_18629nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS18629NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS18629NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jazS_18629nbp/JazS18629NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazS_18629nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS18629NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
