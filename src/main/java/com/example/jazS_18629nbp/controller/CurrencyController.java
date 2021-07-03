2cd3c1a010c650eb17eb513f2074e0be3b16d9d8
s18629 <57294801+s18629@users.noreply.github.com>
2021-07-03 15:23:20 +0200

    Service, Controller, models, swagger, restTemplate

--- src/main/java/com/example/jazS_18629nbp/controller/CurrencyController.java
+++ src/main/java/com/example/jazS_18629nbp/controller/CurrencyController.java
@@ -2,7 +2,10 @@ package com.example.jazS_18629nbp.controller;
 
 import com.example.jazS_18629nbp.service.CurrencyService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class CurrencyController {
@@ -13,6 +16,9 @@ public class CurrencyController {
         this.currencyService = currencyService;
     }
 
-//    @GetMapping("currency/{curr}/{firstDate}/{secondDate}")
-//    public
+        @GetMapping("currency/{curr}/{firstDate}/{secondDate}")
+        public double calculateAverageController(@PathVariable String curr,
+                                                 @PathVariable String firstDate, @PathVariable String secondDate){
+        return currencyService.getAverageCurrency(curr,firstDate,secondDate);
+        }
 }
--- src/main/java/com/example/jazS_18629nbp/service/CurrencyService.java
+++ src/main/java/com/example/jazS_18629nbp/service/CurrencyService.java
@@ -2,27 +2,43 @@ package com.example.jazS_18629nbp.service;
 
 import com.example.jazS_18629nbp.conf.RestTemplateConf;
 import com.example.jazS_18629nbp.model.CurrencyModel;
+import com.example.jazS_18629nbp.model.Rate;
 import com.example.jazS_18629nbp.model.Root;
 import com.example.jazS_18629nbp.repository.CurrencyRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class CurrencyService {
 
     private final RestTemplateConf restTemplateConf;
     private final CurrencyRepository currencyRepository;
 
-
     public CurrencyService(RestTemplateConf restTemplateConf, CurrencyRepository currencyRepository) {
         this.restTemplateConf = restTemplateConf;
         this.currencyRepository = currencyRepository;
     }
 
-//       public Double getAverageCurrency(String currency, String firstDate, String secondDate){
-//       String url = "api.nbp.pl/api/exchangerates/rates/a/" + currency + "/" +firstDate+ "/" +secondDate + "/?format=json";
-//       Root root = restTemplateConf.restTemplate().getForEntity(url, Root.class).getBody();
-//
-//    }
+       public double getAverageCurrency(String currency, String firstDate, String secondDate){
+       String url = "http://api.nbp.pl/api/exchangerates/rates/a/" + currency + "/" +firstDate+ "/" +secondDate + "/?format=json";
+       Root root = restTemplateConf.restTemplate().getForEntity(url, Root.class).getBody();
+
+       return calculateAverage(root.getRates());
+    }
+
+    public double calculateAverage(List<Rate> rateList){
+        double sum = 0;
+        for (Rate rate : rateList){
+            double averageDouble = rate.getMid();
+            double average = averageDouble;
+            sum += average;
+        }
+        return sum/rateList.size();
+    }
+
+
 }
