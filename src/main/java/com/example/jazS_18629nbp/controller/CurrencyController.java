def743394233564f60ea5a3218acc174536946e5
s18629 <57294801+s18629@users.noreply.github.com>
2021-07-03 15:29:50 +0200

    @ApiOperation in controller

--- src/main/java/com/example/jazS_18629nbp/controller/CurrencyController.java
+++ src/main/java/com/example/jazS_18629nbp/controller/CurrencyController.java
@@ -1,6 +1,7 @@
 package com.example.jazS_18629nbp.controller;
 
 import com.example.jazS_18629nbp.service.CurrencyService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class CurrencyController {
     public CurrencyController(CurrencyService currencyService) {
         this.currencyService = currencyService;
     }
-
+        @ApiOperation(value = "CalculateAverageController", notes = "Controller which one return average of currency.")
         @GetMapping("currency/{curr}/{firstDate}/{secondDate}")
         public double calculateAverageController(@PathVariable String curr,
                                                  @PathVariable String firstDate, @PathVariable String secondDate){
